b7abfcf63e2a55c1fd57610994ad5eaeaccfba46
nishan0000 <mdnishan006@gmail.com>
2024-10-14 22:43:52 +0530

    Started Again

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,33 @@
-// Populating the page with data
-let productsHTML = '';
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090
+},{
+  image: 'images/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4.0,
+    count: 127
+  },
+  priceCents: 2095
+}, {
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799
+}];
 
+
+let productsHTML = '';
 products.forEach((product) => {
-  productsHTML += `
+  const html = `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -10,14 +35,14 @@ products.forEach((product) => {
       </div>
 
       <div class="product-name limit-text-to-2-lines">
-        ${product.name}
+      ${product.name}
       </div>
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
           src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-          ${product.rating.count}
+        ${product.rating.count}
         </div>
       </div>
 
@@ -47,43 +72,10 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button 
-      button-primary js-add-to-cart"
-      data-product-Id="${product.id}">
+      <button class="add-to-cart-button button-primary">
         Add to Cart
       </button>
-    </div>
-  `;
-
+    </div>`;
+  productsHTML += html
 })
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-
-// Adding to cart and Related
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-
-      let matchingItem;
-      cart.forEach((item) => {
-        if (productId === item.productId){
-          matchingItem = item;
-        }
-      })
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-      // Making cart quantity interactive
-      let cartQuantity = 0;
-      cart.forEach(item => cartQuantity += item.quantity);
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    });
-  });
-
